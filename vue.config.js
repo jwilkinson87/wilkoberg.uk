192db037ff8f02024a53975fe5605a658ecb09ed
Jonathan Wilkinson <jonny@ctrl-hub.com>
2020-03-13 08:20:17 +0000

    Pre-render routes in production mode

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
+module.exports = {
+    configureWebpack: {
+        plugins: process.env.NODE_ENV === 'production' ? [
+            new PrerenderSPAPlugin({
+                staticDir: path.join(__dirname, 'dist'),
+                routes: ['/', '/about', '/contact']
+            })
+        ]: []
+    }
+}
