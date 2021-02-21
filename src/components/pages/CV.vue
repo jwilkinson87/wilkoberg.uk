a5f27630f86f21e73e11e617be51ced9c36cb751
Jonathan Wilkinson <jonny041187@gmail.com>
2021-02-21 22:22:36 +0000

    Styling updates

--- src/components/pages/CV.vue
+++ src/components/pages/CV.vue
@@ -3,14 +3,18 @@
         <div class="page-title">
             <h1 class="title is-1">Curriculum Vitae</h1>
         </div>
-        <section class="content" v-for="(job, index) in cv" :key="index">
-            <h2 class="is-size-6 has-text-weight-bold job-title">{{ job.job_title }}</h2>
+        <section class="content work-item" v-for="(job, index) in cv" :key="index">
+            <h2 class="is-size-5 has-text-weight-bold job-title">{{ job.job_title }}</h2>
             <div class="work-meta">
                 <p class="company-name has-text-grey dark has-text-weight-400">{{ job.company }}</p>
                 <p class="has-text-grey-dark work-period">{{ job.work_period}}</p>
+                <ul class="list-inline">
+                    <li v-for="(item, index) in job.skills" :key="index" class="tag is-dark">
+                        {{ item }}
+                    </li>
+                </ul>
             </div>
-            <div class="work-description" v-html="nl2br(job.description)">
-            </div>
+            <div class="work-description" v-html="nl2br(job.description)" />
         </section>
     </div>
 </template>
@@ -42,6 +46,12 @@ export default {
 
 <style lang="scss" scoped>
 
+.work-item:not(:last-child) {
+    margin-bottom: 1rem;
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #CCC;
+}
+
 .work-meta {
     p {
         font-size: 0.9rem;
@@ -52,6 +62,22 @@ export default {
 .work-description {
     padding: 10px 0;
     font-size: 1.1rem;
+}
+
+.list-inline {
+    list-style: none;
+    margin: 10px 0;
+    
+    > li {
+        display: inline;
+        font-size: 0.9rem;
+        padding: 5px 10px;
+        border-radius: 5px;
+
+        &:not(:last-child) {
+            margin-right: 0.5rem;
+        }
+    }
 }
 
 </style>
